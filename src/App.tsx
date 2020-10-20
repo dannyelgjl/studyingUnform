31ccb5d6aaa96fb48e5190062d7ac1d8e06427de
dannyelgjl <dannyelgjl@gmail.com>
2020-10-20 16:12:20 -0300

    criando formulário com unForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+
+import Input from "./components/Input/";
 
 function App() {
+  const ref = useRef(null);
+
+  const submit = (data: object) => {
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Form ref={ref} onSubmit={submit}>
+      <Input name="nome" type="text" placeholder="nome" />
+      <Input name="sobrenome" type="text" placeholder="sobrenome" />
+      <Input name="idade" type="text" placeholder="idade" />
+      <Input name="endereço" type="text" placeholder="endereço" />
+      <Input name="rua" type="text" placeholder="rua" />
+
+      <button type="submit">Enviar</button>
+    </Form>
   );
 }
 
